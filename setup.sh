39d56219d108d7ee51e6c7c611e2e953e3389666
hungrydonkey <hungrydonkey@outlook.com>
2025-11-22 06:49:43 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
-
+VENV_DIR=".env"
 # zsh/fish 
 python3 -m venv .env
 
-source .env/bin/activate && pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple -r requirements.txt
+if [ ! -d "$VENV_DIR" ]; then
+  echo "creat $VENV_DIR..."
+  python3 -m venv "$VENV_DIR"
+  source .env/bin/activate && pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple -r requirements.txt
+else
+  echo "$VENV_DIR is already exists."
+fi
 
+source .env/bin/activate
 make
